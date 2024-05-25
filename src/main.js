60a884eef6997b1d5c0dc704eae7f61dcc2d9561
Swathi Rao <128306287+SwathiR03@users.noreply.github.com>
2024-05-25 19:00:02 +0530

    Added Map data in Json/PNG form and updated some code

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import { k } from "./kaboomCtx";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX:39,
-    sliceY:31
+    sliceY:31,
     anims: {
+        "idle-down":960,
+        "walk-down":{from: 960, to: 963, loop:true, speed:7},
+        "idle-side-right":999,
+        "walk-side-right":{from:999, to:1002, loop:true, speed:7},
+        "idle-up":1038,
+        "walk-up":{from:1038, to:1041, loop:true, speed:7},
+        "idle-side-left":1077,
+        "walk-side-left":{from:1077, to:1080, loop:true, speed:7},
+    },
+});
+k.loadSprite("map", "./map.png");
 
-    }
-})
+k.setBackground(k.Color.fromHex("#041E42"));
